80d214c4d2f9dac6bd2b33b3523578d442b206c2
Eni Lec <33154448+Zics@users.noreply.github.com>
2017-11-14 21:46:03 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Pizza</title>
+    </head>
+
+    <body>
+    
+<form action="/action_page.php">
+    <fieldset>
+        <legend>Entrer un utilisateur : </legend>
+        
+        <legend>Nom :<br>
+        <p><label><input type="text" name="firstname" value="">
+        </label></p>    
+      
+        <p><label>Prénom :<br>
+        <input type="text" name="lastname" value="">
+        </label></p>
+        
+        <p><label>Mail :<br>
+        <input type="text" name="mail" value="">
+        </label></p>
+            
+        <p><label>Login :<br>
+        <input type="text" name="login" value="">
+        </label></p>   
+            
+        <p><label>Password :<br>
+        <input type="password" name="passe" value="">
+        </label></p>    
+            
+        <p><label>Confirm password :<br>
+        <input type="password" name="passe2" value="">
+        </label></p>  
+            
+        <input type="submit" value="Valider">
+        
+        
+    </fieldset>
+</form>
+        
+        
+    </body>
+</html>
